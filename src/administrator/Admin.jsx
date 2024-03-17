e181755019539b3f0264d0eba57baaf4d43e3a25
gulamyazdani <gulamyazdani03@gmail.com>
2024-03-17 20:07:07 +0530

    Adding admin details

---
+++ src/administrator/Admin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const admin = () => {
+  return (
+    <>
+    <a className="admin" href="https://www.github.com/iamyazdani" target="_blank">built by - Gulam Yazdani</a>
+    </>
+  );
+};
+
+export { admin };
