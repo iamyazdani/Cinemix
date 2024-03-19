8ebbccd1eb732145115f5c8d79031e131fd5d4d4
gulamyazdani <gulamyazdani03@gmail.com>
2024-03-19 11:35:08 +0530

    Fixing Footer for Admin

--- src/administrator/Admin.jsx
+++ src/administrator/Admin.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Admin = () => {
   return (
     <>
-    <a className="admin" href="https://www.github.com/iamyazdani" target="_blank">built by - Gulam Yazdani</a>
+    <a className="admin" href="https://www.github.com/iamyazdani" target="_blank">Developed by - Gulam Yazdani</a>
+    <span className="admin">&copy; 2024 Google LLC</span>
     </>
   );
 };
